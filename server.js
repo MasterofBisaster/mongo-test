15f0dcab2c398a57c04b446e7ac3fc9715683712
MasterofBisaster <40423782+MasterofBisaster@users.noreply.github.com>
2021-04-26 15:06:56 +0200

    test of webhook

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to the cat db."});
+    res.json({"message": "This seems to work. For meow..."});
 });
 
 require('./app/routes/cat.routes.js')(app);
